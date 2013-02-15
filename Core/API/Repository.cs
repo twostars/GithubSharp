0a74db130bca149eac8106290fda5b050a3e4a72
rhysc <rhysevancampbell@gmail.com>
2013-02-15 15:35:33 +0800

    attempt to add tag search func

--- Core/API/Repository.cs
+++ Core/API/Repository.cs
@@ -15,69 +15,40 @@ namespace GithubSharp.Core.API
         public IEnumerable<Models.RepositoryFromSearch> Search(string search)
         {
             LogProvider.LogMessage(string.Format("Repository.Search - '{0}'", search));
-
-            var url = string.Format("{0}{1}",
-                                    "repos/search/",
-                                    search);
-
+            var url = string.Format("legacy/repos/search/{0}", search);
             var result = ConsumeJsonUrl<Models.Internal.RepositoryCollection<Models.RepositoryFromSearch>>(url);
-
             return result == null ? null : result.Repositories;
         }
 
         public Models.Repository Get(string username, string repositoryName)
         {
-            LogProvider.LogMessage(string.Format("Repository.Get - Username : '{0}' , RepositoryName : '{1}'", username,
-                                                 repositoryName));
-
-            var url = string.Format("{0}/{1}/{2}",
-                                    "repos",
-                                    username,
-                                    repositoryName);
-
+            LogProvider.LogMessage(string.Format("Repository.Get - Username : '{0}' , RepositoryName : '{1}'", username, repositoryName));
+            var url = string.Format("{0}/{1}/{2}", "repos", username, repositoryName);
             var result = ConsumeJsonUrl<Models.Repository>(url);
-
             return result;
         }
 
         public IEnumerable<Models.Repository> List(string username)
         {
             LogProvider.LogMessage(string.Format("Repository.List - Username : '{0}'", username));
-
-            var url = string.Format("{0}{1}",
-                                    "repos/show/",
-                                    username);
-
+            var url = string.Format("{0}{1}", "repos/show/", username);
             var result = ConsumeJsonUrl<Models.Internal.RepositoryCollection<Models.Repository>>(url);
-
             return result == null ? null : result.Repositories;
         }
 
-
-
-
         public IEnumerable<Models.Repository> Network(string repositoryName, string username)
         {
-            LogProvider.LogMessage(string.Format("Repository.Network - RepositoryName : '{0}' , Username : '{1}' ",
-                                                 repositoryName, username));
-
+            LogProvider.LogMessage(string.Format("Repository.Network - RepositoryName : '{0}' , Username : '{1}' ", repositoryName, username));
             var url = string.Format("repos/show/{1}/{0}/network", repositoryName, username);
-
             var result = ConsumeJsonUrl<Models.Internal.RepositoryFromNetworkContainer>(url);
-
             return result == null ? null : result.Network.ToArray();
         }
 
         public IEnumerable<Models.Language> LanguageBreakDown(string repositoryName, string username)
         {
-            LogProvider.LogMessage(
-                string.Format("Repository.LanguageBreakDown - RepositoryName : '{0}' , Username : '{1}' ",
-                              repositoryName, username));
-
+            LogProvider.LogMessage(string.Format("Repository.LanguageBreakDown - RepositoryName : '{0}' , Username : '{1}' ", repositoryName, username));
             var url = string.Format("repos/show/{1}/{0}/languages", repositoryName, username);
-
             var result = ConsumeJsonUrl<Models.Internal.LanguagesCollection>(url);
-
             return result == null
                        ? null
                        : result.Languages.ToList()
@@ -87,13 +58,9 @@ namespace GithubSharp.Core.API
 
         public IEnumerable<Models.TagOrBranch> Tags(string repositoryName, string username)
         {
-            LogProvider.LogMessage(string.Format("Repository.Tags - RepositoryName : '{0}' , Username : '{1}' ",
-                                                 repositoryName, username));
-
-            var url = string.Format("repos/show/{1}/{0}/tags", repositoryName, username);
-
+            LogProvider.LogMessage(string.Format("Repository.Tags - RepositoryName : '{0}' , Username : '{1}' ", repositoryName, username));
+            var url = string.Format("repos/{0}/{1}/tags", username, repositoryName);
             var result = ConsumeJsonUrl<Models.Internal.TagCollection>(url);
-
             return result == null
                        ? null
                        : result.Tags.Dict.ToList()
@@ -103,13 +70,9 @@ namespace GithubSharp.Core.API
 
         public Models.TagOrBranch[] Branches(string repositoryName, string username)
         {
-            LogProvider.LogMessage(string.Format("Repository.Branches - RepositoryName : '{0}' , Username : '{1}' ",
-                                                 repositoryName, username));
-
+            LogProvider.LogMessage(string.Format("Repository.Branches - RepositoryName : '{0}' , Username : '{1}' ", repositoryName, username));
             var url = string.Format("repos/show/{1}/{0}/branches", repositoryName, username);
-
             var result = ConsumeJsonUrl<Models.Internal.BranchesCollection>(url);
-
             return result == null
                        ? null
                        : result.Branches.Dict.Select(p => new Models.TagOrBranch { Name = p.Key, Sha = p.Value })
--- Tests/CoreTests/RepositoryFixture.cs
+++ Tests/CoreTests/RepositoryFixture.cs
@@ -17,9 +17,29 @@ namespace GithubSharp.Tests.CoreTests
         public void CanGetRepository()
         {
             var repoApi = new Repository(new BasicCacher.BasicCacher(), new NullLogger());
-            var repos = repoApi.Get("rhysc", "GithubSharp");
+            var repos = repoApi.Get("rhysc", "GithubSharp");          
             Assert.NotNull(repos);
             Assert.AreEqual("GithubSharp", repos.Name);
+        }
+
+        [Test]
+        public void CanSearchRepositories()
+        {
+            var repoApi = new Repository(new BasicCacher.BasicCacher(), new NullLogger());
+            var repos = repoApi.Search("GithubSharp").ToArray();
+            Assert.NotNull(repos);
+            Assert.IsNotEmpty(repos);
+            Assert.IsNotEmpty(repos.Where(r=>r.Username=="RhysC").ToArray());
+        }
+
+        [Test]
+        public void CanGetRepositoryTags()
+        {
+            var repoApi = new Repository(new BasicCacher.BasicCacher(), new NullLogger());
+            var tags = repoApi.Tags("GithubSharp", "RhysC").ToArray();
+            Assert.NotNull(tags);
+            Assert.IsNotEmpty(tags);
+            Assert.IsNotEmpty(tags.Where(t => t.Name== "DummyTag").ToArray());
         }
     }
 }
